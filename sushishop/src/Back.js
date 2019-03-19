4bc5a6b544fb2e152963f5140442db8e1f17f27c
HakimElkabouss <elkabousshakim@hotmail.com>
2019-03-19 12:35:50 +0100

    modified back

--- sushishop/src/Back.js
+++ sushishop/src/Back.js
@@ -14,25 +14,25 @@ app.use(bodyParser.json());
 
 
 db.serialize(() =>{
-    db.run('CREATE TABLLE IF NOT EXISTS products (product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT, product_img TEXT, product_price INTEGER, product_description TEXT )');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,o?), ("Poke Bowl Saumon", "poke-bowl-saumon-detox.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Poke Bowl Daurade", "poke-bowl-daurade.png", 15, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Maki Yellow Ponzu", "maki-yellowtail-ponzu.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Maki thon spicy", "maki-thon-spicy.png", 15, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Lunch Box 3", "lunch-box-3.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Lunch Box 6", "lunch-box-6.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Gourmet Mix", "gourmet-mix.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("California saumon wasabi", "california-saumon-a-wasabi-de-.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("ceviche de Daurade", "ceviche-de-daurade-a-leche-de-.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Sashimi Yellow", "sashimi-yellowtail.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Sushi Yellow, "sushi-yellowtail.png", 12, "Saumon, Haricot")');
-    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?), ("Sushi Yellow tail Safran, "sushi-yelowtail-safran-a-fleu.png", 12, "Saumon, Haricot")');
+    db.run('CREATE TABLE IF NOT EXISTS products (product_id INTEGER PRIMARY KEY AUTOINCREMENT, product_name TEXT, product_img TEXT, product_price INTEGER, product_description TEXT )');
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Poke Bowl Saumon", "poke-bowl-saumon-detox.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Poke Bowl Daurade", "poke-bowl-daurade.png", 15, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Maki Yellow Ponzu", "maki-yellowtail-ponzu.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Maki thon spicy", "maki-thon-spicy.png", 15, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Lunch Box 3", "lunch-box-3.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Lunch Box 6", "lunch-box-6.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Gourmet Mix", "gourmet-mix.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "California saumon wasabi", "california-saumon-a-wasabi-de-.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "ceviche de Daurade", "ceviche-de-daurade-a-leche-de-.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Sashimi Yellow", "sashimi-yellowtail.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Sushi Yellow", "sushi-yellowtail.png", 12, "Saumon, Haricot");
+    db.run('INSERT INTO products (product_name, product_img, product_price, product_description) VALUES (?,?,?,?)', "Sushi Yellow tail Safran", "sushi-yelowtail-safran-a-fleu.png", 12, "Saumon, Haricot");
 
     
     
     
     
-    db.run('CREATE TABLLE IF NOT EXISTS category (category_id INTEGER PRIMARY KEY AUTOINCREMENT, cartegory_name TEXT, product_id INTEGER, FOREIGN KEY(product_id) REFERENCES product(id) )')
+    db.run('CREATE TABLLE IF NOT EXISTS category (category_id INTEGER PRIMARY KEY AUTOINCREMENT, cartegory_name TEXT, product_id INTEGER, FOREIGN KEY(product_id) REFERENCES products(id) )')
     
     db.run('INSERT INTO category (category_name, product_id) VALUES (?,?)', "Lunch Boxes", 5);
     db.run('INSERT INTO category (category_name, product_id) VALUES (?,?)', "Lunch Boxes", 6);
